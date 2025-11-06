741dfe95b382b6e768363c9dcc0bf49d4bfbd0bd
limelemonade555 <limelemonade555@gmail.com>
2025-11-07 00:50:12 +0700

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const domains = [
-    { name: "startupviet.com", price: 1200 },
-    { name: "techsolutions.vn", price: 800 },
-    { name: "muabanonline.net", price: 600 },
-    { name: "dulichviet.org", price: 950 },
-    { name: "giaoduc24h.com", price: 1100 },
-    { name: "suckoetoan.vn", price: 750 },
-    { name: "thoitrangnam.vn", price: 850 },
-    { name: "batdongsan24h.net", price: 1300 }
+    { name: "gun88.com", price: 20,427 },
+    { name: "fe88.com", price: 23,063 },
+    { name: "men88.com", price: 5,197 },
+    { name: "vec88.com", price: 10,823 },
+    { name: "bun88.com", price: 1,942 },
+    { name: "xom88.com", price: 1,200 },
+    { name: "keo99.com", price: 3,265 },
+    { name: "so88.win", price: 1,215 },
+    { name: "vip99.top", price: 2,093 },
+    { name: "s99.club", price: 1,823 },
+    { name: "xe88.net", price: 3,860 },
+    { name: "bet9.club", price: 5,019 },
+    { name: "z88.top", price: 2,225 },
+    { name: "b88.top", price: 2,808 },
+    { name: "zo88.win", price: 1,956 },
+    { name: "bh88.net", price: 3,236 },
+    { name: "fu88.net", price: 3,478 }
 ];
 
 function displayDomains() {
